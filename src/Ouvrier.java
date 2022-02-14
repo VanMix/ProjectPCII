995a2e9709edbf779a4fe05d9efd9c509a210534
Thomas <Thomas@JP>
2022-02-14 02:49:32 +0100

    Premier essai de déplacement de l'AI

---
+++ src/Ouvrier.java
@@ -0,0 +1,25 @@
+import java.awt.Point;
+
+public class Ouvrier extends Unite{
+	private Point position;
+    
+	public Ouvrier() {
+	}
+	
+	public Ouvrier(Point pos) {
+		position = pos;
+	}
+	
+	@Override
+	void seDeplacer(Point d) {
+		position = d;
+		
+	}
+
+	@Override
+	Point getPos() {
+		return position;
+	}
+	
+
+}
