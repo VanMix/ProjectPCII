e3dc58bfa6b8b2eeab8caa54554427e5aedca5d3
charl <charlieslasluisa@gmail.com>
2022-02-21 02:02:43 +0100

    MAJ Affichage

--- src/Affichage.java
+++ src/Affichage.java
@@ -20,8 +20,12 @@ public class Affichage extends JPanel{
 		super.paint(g);
 		//Utilisation de la classe Graphics2D afin d'effectuer une rotation de la map lors de l'affichage
 		Graphics2D graphs = (Graphics2D) g;
-		graphs.rotate(-Math.PI/4);
-		graphs.translate(-175,150);
+		graphs.rotate(-Math.PI / 4);
+		graphs.translate(-175, 150);
+
+
+		//Affichage des ressources
+		dessineRessource(g);
 
 		/*for(int i = 1; i <= (l.size())/10; i++) {
 			Point rot1 = l.get(0);
@@ -32,46 +36,40 @@ public class Affichage extends JPanel{
 			g.drawLine(p1[0]*decalage, i*decalage, p2[0]*decalage,  i * decalage);
 		}*/
 		this.setBackground(new Color(235, 178, 102));
-		for(ArrayList<Point>ligne : etat.getCarte().getGrille()){
-			for(int i = 1; i <= ligne.size(); i++) {
+		for (ArrayList<Point> ligne : etat.getCarte().getGrille()) {
+			for (int i = 1; i <= ligne.size(); i++) {
 				Point rot1 = ligne.get(0);
 				Point rot2 = ligne.get(ligne.size() - 1);
 
-				g.drawLine(rot1.x * decalage, i * decalage, rot2.x , i * decalage);
+				g.drawLine(rot1.x * decalage, i * decalage, rot2.x, i * decalage);
 				g.drawLine(i * decalage, rot1.y * decalage, i * decalage, rot2.y * decalage);
 			}
 
-	//	ArrayList<Unite> aiList = etat.getAI().getUnit();
-		g.setColor(Color.RED);
-		
-		for(Unite u : aiList) {
-			Point p = u.getPos();
-			g.drawOval(p.x *decalage + 7, p.y * decalage + 7, 20, 20);
+			//	ArrayList<Unite> aiList = etat.getAI().getUnit();
+			g.setColor(Color.RED);
+
+			for (Unite u : aiList) {
+				Point p = u.getPos();
+				g.drawOval(p.x * decalage + 7, p.y * decalage + 7, 20, 20);
+			}
+
 		}
-
-		//Affichage des ressources
-		dessineRessource(g);
 	}
-
-	public void dessineRessource(Graphics g)
-	{
-			for(Ressource r : this.etat.getRessource())
-			{
-				if(r.tR == typeRessource.bois)
-				{
+		public void dessineRessource (Graphics g)
+		{
+			for (Ressource r : this.etat.getRessource()) {
+				if (r.tR == typeRessource.bois) {
 					g.setColor(Color.BLUE);
-					g.drawOval(r.getPosition().x*this.decalage + 15, r.getPosition().y*this.decalage + 15, 5, 5);
-					g.fillOval(r.getPosition().x*this.decalage + 15, r.getPosition().y*this.decalage + 15, 5, 5);
-				}
-				else
-				{
+					g.drawOval(r.getPosition().x * this.decalage + 15, r.getPosition().y * this.decalage + 15, 5, 5);
+					g.fillOval(r.getPosition().x * this.decalage + 15, r.getPosition().y * this.decalage + 15, 5, 5);
+				} else {
 					g.setColor(Color.CYAN);
-					g.drawOval(r.getPosition().x*this.decalage + 15, r.getPosition().y*this.decalage + 15, 5, 5);
-					g.fillOval(r.getPosition().x*this.decalage + 15, r.getPosition().y*this.decalage + 15, 5, 5);
+					g.drawOval(r.getPosition().x * this.decalage + 15, r.getPosition().y * this.decalage + 15, 5, 5);
+					g.fillOval(r.getPosition().x * this.decalage + 15, r.getPosition().y * this.decalage + 15, 5, 5);
 				}
 			}
-	}
-	
+		}
+
 	public int getDecal() {
 		return decalage;
 	}
