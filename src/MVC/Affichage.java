ea1fa4748c0daf98ce6079e814fbd29cf7822ddb
charl <charlieslasluisa@gmail.com>
2022-03-21 15:52:30 +0100

    MAJ affichage

--- src/MVC/Affichage.java
+++ src/MVC/Affichage.java
@@ -7,7 +7,7 @@ import java.awt.*;
 import java.util.ArrayList;
 
 
-public class Affichage extends Grille{
+public class Affichage extends Grille {
 	private final int hauteur = 500;
 	private final int largeur = 800;
 	private Etat etat = new Etat(this);
@@ -15,15 +15,15 @@ public class Affichage extends Grille{
 
 	// Attributs : taille et tableau de cases
 	private Case[][] plateau;
-	
+
 	private ArrayList<Unite> aiList = new ArrayList<>();
 
 	public Affichage(int taille) {
 		super(taille, taille);
 		this.plateau = new Case[taille][taille];
 
-		for(int x = 0; x < plateau.length; x++){
-			for(int y = 0; y < plateau[x].length; y++){
+		for (int x = 0; x < plateau.length; x++) {
+			for (int y = 0; y < plateau[x].length; y++) {
 				this.plateau[x][y] = new Case(etat);
 				ajouteElement(this.plateau[x][y]);
 			}
@@ -31,14 +31,13 @@ public class Affichage extends Grille{
 		setAllRessources();
 		this.setBackground(Color.orange);
 	}
+
 	/**
 	 * Methode pour initialise toute les ressources dans chaque case en fonction des coordonnees de chaque ressources.
 	 */
-	public void setAllRessources()
-	{
-		for(Ressource r : this.etat.getListRessource())
-		{
-			System.out.println("x = "+r.getPosition().x+" "+"y = "+r.getPosition().y);
+	public void setAllRessources() {
+		for (Ressource r : this.etat.getListRessource()) {
+			System.out.println("x = " + r.getPosition().x + " " + "y = " + r.getPosition().y);
 			this.plateau[r.getPosition().x][r.getPosition().y].setRessource(r);
 		}
 	}
@@ -47,7 +46,7 @@ public class Affichage extends Grille{
 		return plateau;
 	}
 
-	public void setCase(Point pos){
+	public void setCase(Point pos) {
 		plateau[pos.x][pos.y] = new Case(etat);
 		ajouteElement(plateau[pos.x][pos.y]);
 	}
@@ -89,13 +88,5 @@ public class Affichage extends Grille{
 
 		}
 	}
-
-
-	public int getDecal() {
-		return decalage;
-	}
-	
-	public void setAIList(ArrayList<Unite> l) {
-		aiList = l;
-	}*/
+	 */
 }
